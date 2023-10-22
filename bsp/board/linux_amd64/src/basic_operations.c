e6b6faa81e33015844a557c69d393926eb39526d
Horned Heck <holitrixpecora@gmail.com>
2023-10-22 21:31:41 +0200

    Cmake presets configured for easy setup of multiple archs

---
+++ bsp/board/linux_amd64/src/basic_operations.c
@@ -0,0 +1,10 @@
+#include "basic_operations.h"
+
+#include "stdio.h"
+#include "unistd.h"
+
+void TogglePin(Pin pin) { printf("Toggle pin %d\n", pin); }
+
+void Delay(uint32_t milis) {
+    usleep(milis * 1000);  // NOLINT
+}
---
+++ bsp/board/linux_amd64/src/init_board.c
@@ -0,0 +1,5 @@
+#include "init_board.h"
+
+#include "stdio.h"
+
+void InitBoard() { printf("Initializing...\n"); }
